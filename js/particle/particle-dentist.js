ea1f4c61c86be9f558a5e6029d4cacbd9a554d2b
pradeepmadireddy <pradeepmadireddy@gmail.com>
2018-01-20 18:19:26 +0530

    first try

---
+++ js/particle/particle-dentist.js
@@ -0,0 +1,242 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+"use strict"
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 193,
+      "density": {
+        "enable": false,
+        "value_area": 1843.9238699953512
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.2,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 4.4159378273686,
+        "size_min": 3.248308849205381,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 400.85301521638075,
+      "color": "#ffffff",
+      "opacity": 0.38481889460772545,
+      "width": 2.0844356791251797
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.413648243462092,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 240.5118091298284,
+        "rotateY": 561.194221302933
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 133.99274002972194,
+        "line_linked": {
+          "opacity": 0.7321306647581907
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 1
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+
+);
+
+particlesJS('particles-2',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 193,
+      "density": {
+        "enable": false,
+        "value_area": 1843.9238699953512
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.2,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 4.4159378273686,
+        "size_min": 3.248308849205381,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 400.85301521638075,
+      "color": "#ffffff",
+      "opacity": 0.38481889460772545,
+      "width": 2.0844356791251797
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.413648243462092,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 240.5118091298284,
+        "rotateY": 561.194221302933
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 133.99274002972194,
+        "line_linked": {
+          "opacity": 0.7321306647581907
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 1
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+
+);
---
+++ js/particle/yoga-bubble.js
@@ -0,0 +1,234 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+"use strict"
+/* Otherwise just put the config content (json): */
+
+particlesJS('particle-line',
+  
+  {
+  "particles": {
+    "number": {
+      "value": 71,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.990734199007641,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.891476416322726,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "size_min": 40,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 128.27296486924183,
+      "color": "#ffffff",
+      "opacity": 1,
+      "width": 0.9620472365193136
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.206824121731046,
+      "direction": "top",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": false
+}
+);
+
+
+
+particlesJS('particle-bubble',
+  
+ {
+  "particles": {
+    "number": {
+      "value": 71,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.990734199007641,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.891476416322726,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "size_min": 40,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 128.27296486924183,
+      "color": "#ffffff",
+      "opacity": 1,
+      "width": 0.9620472365193136
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.206824121731046,
+      "direction": "top",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": false
+}
+
+);
+
+particlesJS('particle-bubble-funfacts',
+  
+ {
+  "particles": {
+    "number": {
+      "value": 71,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.990734199007641,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.891476416322726,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "size_min": 40,
+        "sync": true
+      }
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.206824121731046,
+      "direction": "top",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": false
+}
+
+);
